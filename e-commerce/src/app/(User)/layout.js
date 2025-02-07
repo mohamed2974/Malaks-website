e28ef131a3b75d3e477dba9378be7168ca9141b7
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2025-02-07 20:34:48 +0100

    Füge Checkout-Button-Komponente hinzu und implementiere ThemeProvider für die Client-Seite; aktualisiere Importpfade

--- e-commerce/src/app/(User)/layout.js
+++ e-commerce/src/app/(User)/layout.js
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
+import "@/app/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { Providers } from "../../components/providers";
+import { Providers } from "@/utils/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
