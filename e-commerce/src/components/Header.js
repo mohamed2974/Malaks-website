9247b9476c1a9cf089458d8abf081321d3999a54
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2025-02-07 15:59:46 +0100

    Füge Header-Komponente mit Navigationslinks und Theme-Umschalter hinzu; implementiere ThemeProvider und füge Beispiel-Warenkorb-Daten hinzu

--- e-commerce/src/components/Header.js
+++ e-commerce/src/components/Header.js
@@ -1,14 +1,59 @@
+'use client';
 
+import { FiShoppingBag } from "react-icons/fi";
+import { BsBrightnessHighFill } from "react-icons/bs";
+import { MdNightlight } from "react-icons/md";
+import Link from "next/link";
+import Uls from "@/utils/ULs-component";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
-export default function Header(){
-    return(
-        <header className="flex">
-            <div>
+const nav = [
+    <Link href='/' key='link1'>Link 1</Link>,
+    <Link href='/' key='link2'>Link 2</Link>,
+    <Link href='/' key='link3'>Link 3</Link>,
+    <Link href='/' key='link4'>Link 4</Link>,
+];
+
+export default function Header() {
+    const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    // Verhindert das Rendern, bevor die Komponente auf dem Client gemountet wurde
+    if (!mounted) return null;
+
+    // Nur wenn mounted, dann rendern
+    return (
+        <header className={`flex flex-row justify-between items-center px-16 py-3 text-lg`}>
+            {/* Logo */}
+            <div className="w-1/6">
                 <h1>Logo</h1>
             </div>
-            <div>
-                Link
+
+            {/* nav bar */}
+            <div className="w-4/6 text-center">
+                <Uls array={nav} row={true} style='justify-center' listItemStyle='mx-6 py-2'/>
+            </div>
+
+            <div className="flex justify-end w-1/6 ">
+                {/* theme switcher */}
+                <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className={`mr-4 p-1.5 text-2xl rounded-full ${theme === 'dark' ? 'bg-white' : 'bg-black'}`}>
+                    {theme === "dark" ? (
+                        <BsBrightnessHighFill className="text-yellow-500" />
+                    ) : (
+                        <MdNightlight className="text-white " />
+                    )}
+                </button> 
+
+                {/* warenkorb */}
+                <div className="flex items-center">
+                    <FiShoppingBag className="text-3xl" /> 
+                </div>
             </div>
         </header>
-    )
+    );
 }
---
+++ e-commerce/src/components/providers.js
@@ -0,0 +1,22 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function Providers({ children }) {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true); // Setzt `mounted` auf true, wenn die Komponente auf dem Client geladen ist
+    }, []);
+
+    if (!mounted) {
+        return null; // Verhindert das Rendern der `ThemeProvider`-Komponente auf dem Server
+    }
+
+    return (
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+        </ThemeProvider>
+    );
+}
