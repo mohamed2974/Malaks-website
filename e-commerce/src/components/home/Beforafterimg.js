83470932f9ec277d4ddb5dc67ddd17ecafa2418e
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2025-02-16 21:07:01 +0100

    Aktualisiere Beforafterimg-Komponente: ändere Bild-URLs für den Bildvergleich und optimiere das Layout für bessere Darstellung

--- e-commerce/src/components/home/Beforafterimg.js
+++ e-commerce/src/components/home/Beforafterimg.js
@@ -5,11 +5,11 @@ import ReactCompareImage from 'react-compare-image';
 import { LinearGradient } from 'react-text-gradients';
 
 export default function Beforafterimg() {
-    const FIRST_IMAGE = 'https://smeleshkin.github.io/react-before-after-slider-component//assets/image2.jpg';
-    const SECOND_IMAGE = 'https://smeleshkin.github.io/react-before-after-slider-component//assets/image1.jpg';
+    const FIRST_IMAGE = 'https://ungrxmlu9usc52eq.public.blob.vercel-storage.com/wichtig/airpods2-2GtFAIszASaBtmRI1ISRX7Ht0Ph0eW.jpg';
+    const SECOND_IMAGE = 'https://ungrxmlu9usc52eq.public.blob.vercel-storage.com/wichtig/aripods1-c8YpGeXIldHwKiBh9zj3KJLVPn0Cig.jpg';
 
     return (
-        <section className='mx-auto xl:w-5/6'>
+        <section className='mx-auto w-full xl:w-5/6'>
             <div className="text-center mb-10">
                 <h1 className="text-4xl font-bold">
                     <LinearGradient gradient={["to right", "#374151, #c2410c ,#fb923c"]}>
@@ -18,7 +18,7 @@ export default function Beforafterimg() {
                 </h1>
                 <p className="text-gray-600 dark:text-gray-400 mt-2">Sieh selbst, wie dein AirPods Case mit unserem einzigartigen Design aufgewertet wird!</p>
             </div>
-            <div className='rounded-3xl overflow-hidden'>
+            <div className='rounded-3xl overflow-hidden max-h-[80vh] flex items-center justify-center'>
                 <ReactCompareImage leftImage={FIRST_IMAGE} rightImage={SECOND_IMAGE} leftImageLabel={<LabelImage>Vorher</LabelImage>} rightImageLabel={<LabelImage>Nachher</LabelImage>} />
             </div>
         </section>
