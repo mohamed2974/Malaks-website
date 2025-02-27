f554c8eb701b8ea24503b6f61ac04e6cc00a4842
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2025-02-27 16:33:58 +0100

    Ändere Dateiendungen der Katalogbilder und des Katalogvideos von .jpg und .mp4 zu .JPG und .MP4 für konsistente Bildreferenzen.

--- e-commerce/src/data/componente/katalog.js
+++ e-commerce/src/data/componente/katalog.js
@@ -1,13 +1,13 @@
 
 const bilder = [
-    {img: '/katalog/1.jpg', name: 'Bild 1', className: ''},
-    {img: '/katalog/2.jpg', name: 'Bild 2', className: ''},
-    {img: '/katalog/3.jpg', name: 'Bild 3', className: ''},
-    {img: '/katalog/4.jpg', name: 'Bild 4', className: ''},
-    {img: '/katalog/5.jpg', name: 'Bild 5', className: ''},
-    {img: '/katalog/6.jpg', name: 'Bild 6', className: ''},
-    {img: '/katalog/7.jpg', name: 'Bild 7', className: ''},
-    {img: '/katalog/8.jpg', name: 'Bild 8', className: ''},
+    {img: '/katalog/1.JPG', name: 'Bild 1', className: ''},
+    {img: '/katalog/2.JPG', name: 'Bild 2', className: ''},
+    {img: '/katalog/3.JPG', name: 'Bild 3', className: ''},
+    {img: '/katalog/4.JPG', name: 'Bild 4', className: ''},
+    {img: '/katalog/5.JPG', name: 'Bild 5', className: ''},
+    {img: '/katalog/6.JPG', name: 'Bild 6', className: ''},
+    {img: '/katalog/7.JPG', name: 'Bild 7', className: ''},
+    {img: '/katalog/8.JPG', name: 'Bild 8', className: ''},
 ]
 
 export {bilder}
