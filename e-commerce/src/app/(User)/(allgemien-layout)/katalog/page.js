936c988981b9f912d6ff45a8f46fc092d981c00a
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2025-02-27 17:14:28 +0100

    Bereinige den Katalogcode, indem überflüssige Fragment-Wrapper entfernt und die Videoanzeige optimiert wird.

--- e-commerce/src/app/(User)/(allgemien-layout)/katalog/page.js
+++ e-commerce/src/app/(User)/(allgemien-layout)/katalog/page.js
@@ -13,21 +13,19 @@ export default function katalog(){
             </div>
             <div className="grid grid-rows-3 grid-cols-3 md:grid-cols-4 gap-4 p-4">
                 {bilder.map((bild, index) => (
-                    <>
                     <div key={index} className={`${bild.className} relative aspect-square bg-gray-200 rounded-lg overflow-hidden `}>
-                    <Image
-                        src={bild.img}
-                        alt={bild.name}
-                        fill
-                        sizes="(max-width: 768px) 100vw, 50vw" 
-                        className={`hover:scale-105 transition-transform duration-300 object-cover`}
-                    />
+                        <Image
+                            src={bild.img}
+                            alt={bild.name}
+                            fill
+                            sizes="(max-width: 768px) 100vw, 50vw" 
+                            className={`hover:scale-105 transition-transform duration-300 object-cover`}
+                        />
                     </div>
-                    <video autoPlay loop muted playsInline className='aspect-square row-start-2 row-end-4 col-start-2 col-end-4 rounded-3xl'>
-                        <source src="/katalog/katalogvideo.MP4" type="video/mp4"/>
-                    </video>
-                    </>
                 ))}
+                <video autoPlay loop muted playsInline className='aspect-square row-start-2 row-end-4 col-start-2 col-end-4 rounded-3xl'>
+                    <source src="/katalog/katalogvideo.MP4" type="video/mp4"/>
+                </video>
             </div>
             <div className='flex justify-center'>
                 <Link href='/shop' className="inline-block mt-4 bg-BrandBlue hover:bg-BrandBlueLight text-BrandWhite px-6 py-2 rounded-md transition duration-200 shadow-md">
