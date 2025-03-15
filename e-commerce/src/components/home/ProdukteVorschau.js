2bc48be1eccbd4f5234f762c5ce87370bcd51a40
mohamed2974 <122312919+mohamed2974@users.noreply.github.com>
2025-03-15 19:57:33 +0100

    Reduziere die minimale Höhe der Produktvorschau-Sektion auf 40vh für eine bessere Darstellung

--- e-commerce/src/components/home/ProdukteVorschau.js
+++ e-commerce/src/components/home/ProdukteVorschau.js
@@ -3,7 +3,7 @@ import Produktekarten from '../../utils/Produktekarten';
 
 export default function Produkte() {
     return (
-        <section className="min-h-[80vh]">
+        <section className="min-h-[40vh]">
             {/* Header */}
             <div className="text-center mb-10">
                 <GradientTitel text="Unsere Produkte" />
